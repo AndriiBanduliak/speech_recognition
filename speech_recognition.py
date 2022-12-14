52fccc94e1cd07d2722ad0cf0e3c63bbc1db19fa
Andrii Banduliak <banduliak1303@gmail.com>
2022-12-14 12:41:24 +0300

    speech recognition

--- speech_recognition.py
+++ speech_recognition.py
@@ -20,6 +20,3 @@ except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
-
